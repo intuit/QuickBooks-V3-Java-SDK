79ff0c24cd251a1d4f017f66429a701997e17938
Diana De Rose <diana_derose@intuit.com>
2017-06-20 09:57:02 -0700

    add copyright

--- ipp-v3-java-data/src/main/java/com/intuit/sb/cdm/util/v3/TimestampAdapter.java
+++ ipp-v3-java-data/src/main/java/com/intuit/sb/cdm/util/v3/TimestampAdapter.java
@@ -1,9 +1,18 @@
-/* CONFIDENTIAL -- Copyright 2009, 2017 Intuit Inc. This material contains certain  *
- * trade secrets and confidential and proprietary information of Intuit Inc.  *
- * Use, reproduction, disclosure and distribution by any means are prohibited,*
- * except pursuant to a written license from Intuit Inc. Use of copyright     *
- * notice is precautionary and does not imply publication or disclosure.      *
- */
+/*******************************************************************************
+ * Copyright (c) 2017 Intuit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package com.intuit.sb.cdm.util.v3;
 
 import java.sql.Timestamp;
