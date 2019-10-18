dac578178469fe32ce8fe856bf33b96b6753c3e0
prao2 <praveena_rao@intuit.com>
2019-10-17 19:35:40 -0700

    #60 - Added copyright

--- payments-api/src/test/java/com/intuit/payment/http/RequestTest.java
+++ payments-api/src/test/java/com/intuit/payment/http/RequestTest.java
@@ -4,9 +4,23 @@ import com.intuit.payment.config.RequestContext;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-/**
+/*******************************************************************************
+ * Copyright (c) 2019 Intuit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * @author praveenadg
- */
+ *******************************************************************************/
+
 public class RequestTest {
     private static final String ACCESS_TOKEN = "intuit123";
     private static final String PROD_BASE_URL = "https://api.intuit.com/quickbooks/v4/payments/";
--- payments-api/src/test/java/com/intuit/payment/http/ResponseTest.java
+++ payments-api/src/test/java/com/intuit/payment/http/ResponseTest.java
@@ -4,9 +4,23 @@ import com.intuit.payment.data.Errors;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-/**
+/*******************************************************************************
+ * Copyright (c) 2019 Intuit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * @author praveenadg
- */
+ *******************************************************************************/
+
 public class ResponseTest {
     private static final int STATUS_CODE = 200;
     private static final String INTUIT_TID = "intuit_tid";
