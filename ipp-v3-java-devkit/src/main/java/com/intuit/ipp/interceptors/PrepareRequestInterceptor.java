3b8871335f951167a3482fb865a014add2305b0a
Shweta Kumar <shweta_kumar@intuit.com>
2020-05-07 16:06:17 -0700

    schema updates
    Schema updates to add County field

--- ipp-v3-java-devkit/src/main/java/com/intuit/ipp/interceptors/PrepareRequestInterceptor.java
+++ ipp-v3-java-devkit/src/main/java/com/intuit/ipp/interceptors/PrepareRequestInterceptor.java
@@ -310,7 +310,7 @@ public class PrepareRequestInterceptor implements Interceptor {
 		
 		if(context.getMinorVersion() == null)
 		{
-		context.setMinorVersion("47");
+		context.setMinorVersion("50");
 		}
 		
 		uri.append("minorversion").append("=").append(context.getMinorVersion()).append("&");
