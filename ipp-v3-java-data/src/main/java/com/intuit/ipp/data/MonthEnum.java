97bd9cade7a18864550662811c0c3b658d95ce43
mkiran <manika_kiran@intuit.com>
2025-06-03 15:12:42 +0530

    fix: update vulnerable dependency to resolve CVE

--- ipp-v3-java-data/src/main/java/com/intuit/ipp/data/MonthEnum.java
+++ ipp-v3-java-data/src/main/java/com/intuit/ipp/data/MonthEnum.java
@@ -41,7 +41,7 @@ import javax.xml.bind.annotation.XmlType;
 @XmlEnum
 public enum MonthEnum {
 
-    @XmlEnumValue("JANUARY")
+    @XmlEnumValue("January")
     JANUARY("January"),
     @XmlEnumValue("February")
     FEBRUARY("February"),
